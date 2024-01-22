5a8a3d4c9fa3e3f5dd27db1fd144e1ba8a9bfb01
Ayaan <onestar7984@gmail.com>
2024-01-22 09:31:30 +0900

    feat: LogoWidget Logo Assets 적용

--- lib/presentation/common/logo_widget.dart
+++ lib/presentation/common/logo_widget.dart
@@ -2,15 +2,35 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+enum Logo { white, basic, black }
+
 final class LogoWidget extends StatelessWidget {
+  static final Image _whiteLogo = Image.asset('assets/icons/logo_w.png');
+  static final Image _basicLogo = Image.asset('assets/icons/logo.png');
+  static final Image _blackLogo = Image.asset('assets/icons/logo_b.png');
+
+  final Logo _logo;
   final double _logoHeight;
   final double _logoWidth;
 
+  Image get _logoImage {
+    switch (_logo) {
+      case Logo.white:
+        return _whiteLogo;
+      case Logo.basic:
+        return _basicLogo;
+      case Logo.black:
+        return _blackLogo;
+    }
+  }
+
   const LogoWidget({
     super.key,
+    Logo logo = Logo.basic,
     required double logoHeight,
     required double logoWidth,
-  })  : _logoHeight = logoHeight,
+  })  : _logo = logo,
+        _logoHeight = logoHeight,
         _logoWidth = logoWidth;
 
   @override
@@ -21,8 +41,8 @@ final class LogoWidget extends StatelessWidget {
           child: SizedBox(
             height: min(constraints.maxHeight, _logoHeight),
             width: min(constraints.maxWidth, _logoWidth),
-            child: const FittedBox(
-              child: Icon(Icons.breakfast_dining),
+            child: FittedBox(
+              child: _logoImage,
             ),
           ),
         );
