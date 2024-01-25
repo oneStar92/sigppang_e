376d575ea33d8144bd9bbc7e3a25fa09b5888505
Ayaan <onestar7984@gmail.com>
2024-01-25 09:22:56 +0900

    refactor: main Local 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('ko');
   runApp(const MyApp());
 }
 
