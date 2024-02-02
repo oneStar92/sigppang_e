94e9122483f5bf073d26f15b6a8f099b0fd44ac7
Ayaan <onestar7984@gmail.com>
2024-02-02 19:31:39 +0900

    refactor: CalendarScreen Scaffold 제거

--- lib/presentation/calendar/calendar_screen.dart
+++ lib/presentation/calendar/calendar_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:sigppang_e/presentation/common/sigppang_e_logo.dart';
 import 'package:sigppang_e/common/constants/sizes.dart';
 import 'package:sigppang_e/presentation/calendar/widgets/sigppang_e_calendar/sigppang_e_calendar_widget.dart';
 import 'package:sigppang_e/presentation/calendar/widgets/to_do_list.dart';
@@ -10,24 +8,14 @@ final class CalendarScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Padding(
-          padding: REdgeInsets.only(left: 16),
-          child: SigppangELogo.build(size: Sizes.appbarLogoSize),
-        ),
-        actions: [PopupMenuButton(itemBuilder: (_) => [])],
-      ),
-      body: SingleChildScrollView(
-        padding: REdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SigppangECalendar(),
-            SizedBox(height: 16.h),
-            ToDoList(title: 'To Do')
-          ],
-        ),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SigppangECalendar(),
+          SizedBox(height: Sizes.defaultPaddingOfHeight * 2),
+          ToDoList(title: 'To Do'),
+        ],
       ),
     );
   }
