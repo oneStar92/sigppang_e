eeb3e0957b30e7327ea08b1d313a8248df24bd0c
Ayaan <onestar7984@gmail.com>
2024-02-02 19:31:57 +0900

    refactor: SettingScreen Scaffold 제거

--- lib/presentation/setting/setting_screen.dart
+++ lib/presentation/setting/setting_screen.dart
@@ -10,71 +10,64 @@ final class SettingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final buttonSize = Sizes.settingButtonSize;
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: Sizes.defaultAppPadding,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        const ProfileWidget(),
+        SizedBox(
+          height: buttonSize.height,
+          width: buttonSize.width,
+          child: IconTextButton(
+            icon: const Icon(Icons.person, color: Colors.black),
+            title: '회원가입',
+            color: Colors.black,
+            onTap: () {},
+          ),
+        ),
+        SizedBox(
+          height: buttonSize.height,
+          width: buttonSize.width,
+          child: IconTextButton(
+            icon: const Icon(Icons.logout, color: Colors.red),
+            title: '로그아웃',
+            color: Colors.red,
+            onTap: () {},
+          ),
+        ),
+        SizedBox(
+          height: buttonSize.height,
+          width: buttonSize.width,
+          child: IconTextButton(
+            icon: const Icon(Icons.person_remove, color: Colors.red),
+            title: '회원 탈퇴',
+            color: Colors.red,
+            onTap: () {},
+          ),
+        ),
+        const Spacer(),
+        SizedBox(
+          height: buttonSize.height,
+          width: buttonSize.width,
+          child: Row(
             children: [
-              const ProfileWidget(),
-              SizedBox(
-                height: buttonSize.height,
-                width: buttonSize.width,
-                child: IconTextButton(
-                  icon: const Icon(Icons.person, color: Colors.black),
-                  title: '회원가입',
-                  color: Colors.black,
-                  onTap: () {},
+              Expanded(
+                child: Text(
+                  '버전',
+                  textAlign: TextAlign.left,
+                  style: TextStyles.settingButtonTextStyle.copyWith(fontWeight: FontWeight.w700),
                 ),
               ),
-              SizedBox(
-                height: buttonSize.height,
-                width: buttonSize.width,
-                child: IconTextButton(
-                  icon: const Icon(Icons.logout, color: Colors.red),
-                  title: '로그아웃',
-                  color: Colors.red,
-                  onTap: () {},
-                ),
-              ),
-              SizedBox(
-                height: buttonSize.height,
-                width: buttonSize.width,
-                child: IconTextButton(
-                  icon: const Icon(Icons.person_remove, color: Colors.red),
-                  title: '회원 탈퇴',
-                  color: Colors.red,
-                  onTap: () {},
-                ),
-              ),
-              const Spacer(),
-              SizedBox(
-                height: buttonSize.height,
-                width: buttonSize.width,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Text(
-                        '버전',
-                        textAlign: TextAlign.left,
-                        style: TextStyles.settingButtonTextStyle.copyWith(fontWeight: FontWeight.w700),
-                      ),
-                    ),
-                    Expanded(
-                      child: Text(
-                        '1.0.0',
-                        textAlign: TextAlign.right,
-                        style: TextStyles.settingButtonTextStyle.copyWith(fontWeight: FontWeight.w700),
-                      ),
-                    ),
-                  ],
+              Expanded(
+                child: Text(
+                  '1.0.0',
+                  textAlign: TextAlign.right,
+                  style: TextStyles.settingButtonTextStyle.copyWith(fontWeight: FontWeight.w700),
                 ),
               ),
             ],
           ),
         ),
-      ),
+      ],
     );
   }
 }
