55be4002e119887ca90fae8f8b5d4b7ccdef7b81
Ayaan <onestar7984@gmail.com>
2024-02-22 20:06:13 +0900

    refactor: LoginScreen BaseView Extends

--- lib/presentation/login/login_screen.dart
+++ lib/presentation/login/login_screen.dart
@@ -3,49 +3,62 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' as foundation;
 import 'package:sigppang_e/common/constants/text_styles.dart';
 import 'package:sigppang_e/common/constants/sizes.dart';
+import 'package:sigppang_e/presentation/common/base_view.dart';
 import 'package:sigppang_e/presentation/common/sigppang_e_logo_builder.dart';
+import 'package:sigppang_e/presentation/login/login_view_model.dart';
 import 'package:sigppang_e/presentation/login/widgets/social_login_button.dart';
 
-final class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+final class LoginScreen extends BaseView<LoginViewModel> {
+  const LoginScreen({
+    super.key,
+    required super.viewModel,
+  });
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(child: SigppangELogoBuilder.build(size: Sizes.socialLoginLogoSize)),
-            Expanded(child: _socialLoginButtons()),
-          ],
-        ),
-      ),
+  LoginScreenState createState() => LoginScreenState();
+}
+
+final class LoginScreenState extends BaseViewState<LoginViewModel, LoginScreen> {
+  @override
+  Widget createBody(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(child: SigppangELogoBuilder.build(size: Sizes.socialLoginLogoSize)),
+        Expanded(child: _socialLoginButtons()),
+      ],
     );
   }
 
   Widget _socialLoginButtons() {
     final size = Sizes.socialLoginButtonSize;
     final isiOS = foundation.defaultTargetPlatform == foundation.TargetPlatform.iOS;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(
+    return Padding(
+      padding: Sizes.defaultHorizontalPadding * 2,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
             height: size.height,
-            width: size.width,
-            child: SocialLoginButton(onTap: () {}, socialLogin: SocialLogin.google)),
-        isiOS ? SizedBox(height: Sizes.defaultPaddingOfHeight * 2) : Container(),
-        isiOS
-            ? SizedBox(
-            height: size.height,
-            width: size.width,
-            child: SocialLoginButton(onTap: () {}, socialLogin: SocialLogin.apple))
-            : Container(),
-        SizedBox(height: Sizes.defaultPaddingOfHeight),
-        SizedBox(width: size.width, child: _policyText()),
-      ],
+            child: SocialLoginButton(
+              onTap: () {},
+              socialLogin: SocialLogin.google,
+            ),
+          ),
+          isiOS ? SizedBox(height: Sizes.defaultPaddingOfHeight * 2) : Container(),
+          isiOS
+              ? SizedBox(
+                  height: size.height,
+                  child: SocialLoginButton(
+                    onTap: () {},
+                    socialLogin: SocialLogin.apple,
+                  ),
+                )
+              : Container(),
+          SizedBox(height: Sizes.defaultPaddingOfHeight),
+          _policyText(),
+        ],
+      ),
     );
   }
 
