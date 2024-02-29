e98c41815848e24974edb2ad38ca15222466dc8d
Ayaan <onestar7984@gmail.com>
2024-02-29 20:11:42 +0900

    refactor: ViewModel activityTracker 기능 추가

--- lib/presentation/common/view_model.dart
+++ lib/presentation/common/view_model.dart
@@ -1,5 +1,18 @@
-abstract interface class ViewModel {
-  Stream<bool> get isLoadingStream;
+import 'dart:async';
+import 'package:sigppang_e/presentation/common/activity_tracker.dart';
 
-  Future<void> initState();
+abstract class ViewModel {
+  final ActivityTracker activityTracker;
+
+  Stream<bool> get isLoadingStream => activityTracker.isInactivity;
+
+  ViewModel({
+    required this.activityTracker,
+  });
+
+  initState() {}
+
+  dispose() {
+    activityTracker.dispose();
+  }
 }
