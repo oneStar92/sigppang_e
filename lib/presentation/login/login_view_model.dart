8e8e11b3cdb1a3adcc645a29aeaae2756e25d8f1
Ayaan <onestar7984@gmail.com>
2024-02-29 20:12:03 +0900

    refactor: LoginViewModel ViewModel 변경 사항 반영

--- lib/presentation/login/login_view_model.dart
+++ lib/presentation/login/login_view_model.dart
@@ -1,12 +1,7 @@
-import 'package:rxdart/rxdart.dart';
 import 'package:sigppang_e/presentation/common/view_model.dart';
 
-final class LoginViewModel implements ViewModel {
-  final PublishSubject<bool> _isLoadingSubject = PublishSubject();
-
-  @override
-  Future<void> initState() => Future.value();
-
-  @override
-  Stream<bool> get isLoadingStream => _isLoadingSubject.stream;
+final class LoginViewModel extends ViewModel {
+  LoginViewModel({
+    required super.activityTracker,
+  });
 }
