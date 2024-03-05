a6049d6ec1ce3fe2c899f9d76a2d65c7fb25a6f9
Ayaan <onestar7984@gmail.com>
2024-03-05 15:57:16 +0900

    refactor: LoginViewModel act 틀 구현

--- lib/presentation/login/login_view_model.dart
+++ lib/presentation/login/login_view_model.dart
@@ -1,7 +1,26 @@
 import 'package:sigppang_e/presentation/common/view_model.dart';
+import 'package:sigppang_e/presentation/login/model/login_screen_action.dart';
 
-final class LoginViewModel extends ViewModel {
+final class LoginViewModel extends ViewModel<LoginScreenAction> {
   LoginViewModel({
     required super.activityTracker,
   });
+
+  @override
+  act(LoginScreenAction action) {
+    switch (action) {
+      case GoogleLogin():
+        _googleLogin();
+      case AppleLogin():
+        _appleLogin();
+      case GuestLogin():
+        _guestLogin();
+    }
+  }
+
+  _googleLogin() {}
+
+  _appleLogin() {}
+
+  _guestLogin() {}
 }
