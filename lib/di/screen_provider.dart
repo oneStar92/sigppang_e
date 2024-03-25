6b77603ed8e13dac8348ef5ad38068b495a14857
Ayaan <onestar7984@gmail.com>
2024-03-25 15:32:20 +0900

    feat: ScreenProvider 정의 및 buildLoginScreen 메서드 구현

---
+++ lib/di/screen_provider.dart
@@ -0,0 +1,22 @@
+import 'package:sigppang_e/data/repository/firebase_apple_auth_repository_impl.dart';
+import 'package:sigppang_e/data/repository/firebase_google_auth_repository_impl.dart';
+import 'package:sigppang_e/domain/use_case/firebase_apple_login_use_case.dart';
+import 'package:sigppang_e/domain/use_case/firebase_google_login_use_case.dart';
+import 'package:sigppang_e/presentation/login/login_screen.dart';
+import 'package:sigppang_e/presentation/login/login_view_model.dart';
+
+final class ScreenProvider {
+  ScreenProvider._internal();
+
+  static LoginScreen buildLoginScreen() {
+    final googleAuthRepository = FirebaseGoogleAuthRepositoryImpl();
+    final appleAuthRepository = FirebaseAppleAuthRepositoryImpl();
+    final googleLoginUseCase = FirebaseGoogleLoginUseCase(firebaseGoogleAuthRepository: googleAuthRepository);
+    final appleLoginUseCase = FirebaseAppleLoginUseCase(firebaseAppleAuthRepository: appleAuthRepository);
+    final viewModel = LoginViewModel(
+      googleLoginUseCase: googleLoginUseCase,
+      appleLoginUseCase: appleLoginUseCase,
+    );
+    return LoginScreen(viewModel: viewModel);
+  }
+}
