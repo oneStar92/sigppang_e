306d858c9568aeb97f7f94baa1213516f55de534
Ayaan <onestar7984@gmail.com>
2024-03-25 15:30:08 +0900

    feat: ViewModel ActStreamController 추가 및 act 기능 구현

--- lib/presentation/common/view_model.dart
+++ lib/presentation/common/view_model.dart
@@ -3,19 +3,19 @@ import 'package:sigppang_e/presentation/util/activity_tracker.dart';
 import 'package:sigppang_e/presentation/common/screen_action.dart';
 
 abstract class ViewModel<T extends ScreenAction> {
-  final ActivityTracker activityTracker;
+  final ActivityTracker activityTracker = ActivityTracker();
+  final StreamController<T> _actionController = StreamController<T>.broadcast();
 
   Stream<bool> get isLoadingStream => activityTracker.isInactivity;
 
-  ViewModel({
-    required this.activityTracker,
-  });
+  Stream<T> get actionStream => _actionController.stream;
 
   initState() {}
 
   dispose() {
     activityTracker.dispose();
+    _actionController.close();
   }
 
-  act(T action);
+  act(T action) => _actionController.add(action);
 }
