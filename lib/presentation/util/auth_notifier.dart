c9344dce071bded4fc8f8c8a8ec187916eefa766
Ayaan <onestar7984@gmail.com>
2024-03-26 09:34:06 +0900

    feat: AuthNotifier 구현

---
+++ lib/presentation/util/auth_notifier.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+
+final class AuthNotifier extends ChangeNotifier {
+  User? _user = FirebaseAuth.instance.currentUser;
+
+  static final AuthNotifier instance = AuthNotifier._internal();
+
+  factory AuthNotifier() => instance;
+
+  AuthNotifier._internal() {
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      _user = user;
+      notifyListeners();
+    });
+  }
+
+  User? get currentUser => _user;
+}
