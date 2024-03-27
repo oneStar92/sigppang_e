68f3df3a64b86b558f985222f3af3d74e1ade283
Ayaan <onestar7984@gmail.com>
2024-03-27 09:36:15 +0900

    refactor: ScreenProvider Calendar, Setting, Home Screen Build 메서드 구현

--- lib/di/screen_provider.dart
+++ lib/di/screen_provider.dart
@@ -1,10 +1,13 @@
+import 'package:go_router/go_router.dart';
 import 'package:sigppang_e/data/repository/firebase_apple_auth_repository_impl.dart';
 import 'package:sigppang_e/data/repository/firebase_google_auth_repository_impl.dart';
 import 'package:sigppang_e/domain/use_case/firebase_apple_login_use_case.dart';
 import 'package:sigppang_e/domain/use_case/firebase_google_login_use_case.dart';
+import 'package:sigppang_e/presentation/calendar/calendar_screen.dart';
 import 'package:sigppang_e/presentation/home/home_screen.dart';
 import 'package:sigppang_e/presentation/login/login_screen.dart';
 import 'package:sigppang_e/presentation/login/login_view_model.dart';
+import 'package:sigppang_e/presentation/setting/setting_screen.dart';
 
 final class ScreenProvider {
   ScreenProvider._internal();
@@ -21,7 +24,15 @@ final class ScreenProvider {
     return LoginScreen(viewModel: viewModel);
   }
 
-  static HomeScreen buildHomeScreen() {
-    return HomeScreen();
+  static HomeScreen buildHomeScreen(StatefulNavigationShell navigationShell) {
+    return HomeScreen(navigationShell: navigationShell);
+  }
+
+  static CalendarScreen buildCalendarScreen() {
+    return CalendarScreen();
+  }
+
+  static SettingScreen buildSettingScreen() {
+    return SettingScreen();
   }
 }
