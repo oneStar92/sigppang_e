dd59c11fab6700564cb6bdd02f99f3eaf0695ae1
Ayaan <onestar7984@gmail.com>
2024-03-29 14:46:21 +0900

    refactor: ToDoItem 기능 구현

--- lib/presentation/calendar/widgets/to_do_item.dart
+++ lib/presentation/calendar/widgets/to_do_item.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sigppang_e/common/constants/sizes.dart';
 import 'package:sigppang_e/common/constants/text_styles.dart';
 import 'package:sigppang_e/presentation/calendar/widgets/sigppang_e_checkbox.dart';
 
 final class ToDoItem extends StatelessWidget {
+  final TextEditingController _controller;
+  final FocusNode _focusNode = FocusNode();
   final String _hint = '일정을 입력해 주세요.';
+  final Widget _checkBoxIcon;
+  final Function()? _onChecked;
+  final Function()? _onDeleted;
+  final Function()? _onSaved;
 
-  const ToDoItem({super.key});
+  ToDoItem({
+    super.key,
+    required TextEditingController controller,
+    required Widget checkBoxIcon,
+    Function()? onChecked,
+    Function()? onDeleted,
+    Function()? onSaved,
+  })  : _controller = controller,
+        _checkBoxIcon = checkBoxIcon,
+        _onChecked = onChecked,
+        _onDeleted = onDeleted,
+        _onSaved = onSaved {
+    _focusNode.addListener(() {
+      if (!_focusNode.hasFocus && _controller.text.isNotEmpty) _onSaved?.call();
+      if (!_focusNode.hasFocus && _controller.text.isEmpty) _onDeleted?.call();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,20 +41,26 @@ final class ToDoItem extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SigppangECheckbox(onTap: () {}),
+          SigppangECheckbox(onTap: () => _onChecked?.call(), child: _checkBoxIcon),
           SizedBox(width: Sizes.defaultPaddingOfWidth),
           Expanded(
             child: TextFormField(
               maxLines: 1,
-              initialValue: 'To Do Item',
+              maxLength: 20,
+              maxLengthEnforcement: MaxLengthEnforcement.enforced,
+              autofocus: _controller.text.isEmpty,
+              controller: _controller,
               style: TextStyles.toDoItemTextStyle,
               textAlignVertical: TextAlignVertical.center,
               decoration: InputDecoration.collapsed(hintText: _hint),
+              focusNode: _focusNode,
+              onTapOutside: (_) => _focusNode.unfocus(),
+              onEditingComplete: () => _focusNode.unfocus(),
             ),
           ),
           SizedBox(width: Sizes.defaultPaddingOfWidth),
           IconButton(
-            onPressed: () {},
+            onPressed: _onDeleted,
             icon: const Icon(Icons.delete),
             padding: EdgeInsets.zero,
             iconSize: size.height * 0.7,
