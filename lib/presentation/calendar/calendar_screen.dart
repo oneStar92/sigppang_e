4284839947ff67f77a8baf30216084599bfd974e
Ayaan <onestar7984@gmail.com>
2024-03-29 14:46:32 +0900

    refactor: CalendarScreen 기능 구현

--- lib/presentation/calendar/calendar_screen.dart
+++ lib/presentation/calendar/calendar_screen.dart
@@ -1,44 +1,69 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:sigppang_e/common/constants/sizes.dart';
 import 'package:sigppang_e/common/constants/text_styles.dart';
+import 'package:sigppang_e/domain/model/to_do.dart';
+import 'package:sigppang_e/presentation/calendar/calednar_view_model.dart';
 import 'package:sigppang_e/presentation/calendar/widgets/sigppang_e_calendar.dart';
 import 'package:sigppang_e/presentation/calendar/widgets/to_do_item.dart';
+import 'package:sigppang_e/presentation/common/base_view.dart';
+import 'package:sigppang_e/presentation/util/sigppang_e_logo_builder.dart';
 
-final class CalendarScreen extends StatelessWidget {
-  const CalendarScreen({super.key});
+final class CalendarScreen extends BaseView<CalendarViewModel> {
+  const CalendarScreen({
+    super.key,
+    required super.viewModel,
+  });
 
   @override
-  Widget build(BuildContext context) {
+  BaseViewState<CalendarViewModel, CalendarScreen> createState() => _CalendarScreenState();
+}
+
+final class _CalendarScreenState extends BaseViewState<CalendarViewModel, CalendarScreen> {
+  @override
+  Widget createBody(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          _calendarTitle(),
+          _buildCalendarTitle(),
           SizedBox(height: Sizes.defaultPaddingOfHeight),
-          SigppangECalendar(),
+          _buildCalendar(),
           SizedBox(height: Sizes.defaultPaddingOfHeight * 2),
-          _toDoTitle(),
+          _buildToDoTitle(),
           SizedBox(height: Sizes.defaultPaddingOfHeight),
-          _toDoList(),
+          _buildToDoList(),
         ],
       ),
     );
   }
 
-  Widget _calendarTitle() {
+  Widget _buildCalendarTitle() {
     final size = Sizes.calendarHeaderSize;
     return SizedBox(
       height: size.height,
       width: size.width,
       child: Row(
         children: [
-          Text(
-            '2024년 1월',
-            style: TextStyles.calendarHeaderTextStyle,
+          StreamBuilder(
+            stream: viewModel.title,
+            builder: (context, snapshot) {
+              return Text(
+                snapshot.data ?? '',
+                style: TextStyles.calendarHeaderTextStyle,
+              );
+            },
           ),
           IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.unfold_less),
+            onPressed: () => viewModel.act(CalendarScreenAction.changeFormat()),
+            icon: StreamBuilder(
+              stream: viewModel.isMonthFormat,
+              builder: (_, snapshot) {
+                if (!snapshot.hasData) return const Icon(Icons.unfold_less);
+                return snapshot.requireData ? const Icon(Icons.unfold_less) : const Icon(Icons.unfold_more);
+              },
+            ),
             padding: EdgeInsets.zero,
             iconSize: size.height * 0.7,
             color: Colors.black,
@@ -48,7 +73,44 @@ final class CalendarScreen extends StatelessWidget {
     );
   }
 
-  Widget _toDoTitle() {
+  Widget _buildCalendar() {
+    return StreamBuilder(
+      stream: viewModel.calendar,
+      builder: (context, snapshot) {
+        final defaultIcon = SigppangELogoBuilder.build(
+          size: Sizes.calendarDayLogoSize,
+          state: ToDoState.ready,
+        );
+        final doneIcon = SigppangELogoBuilder.build(size: Sizes.calendarDayLogoSize);
+        final unfinishedIcon = SigppangELogoBuilder.build(
+          size: Sizes.calendarDayLogoSize,
+          state: ToDoState.unfinished,
+        );
+
+        if (!snapshot.hasData) {
+          return SigppangECalendar(
+            events: LinkedHashMap(),
+            defaultIcon: defaultIcon,
+            doneIcon: doneIcon,
+            unfinishedIcon: unfinishedIcon,
+          );
+        }
+        return SigppangECalendar(
+          focusedDay: snapshot.requireData.focusedDay,
+          selectedDay: snapshot.requireData.selectedDay,
+          format: snapshot.requireData.format,
+          onPageChanged: (date) => viewModel.act(CalendarScreenAction.onPageChanged(date)),
+          onDaySelected: (date) => viewModel.act(CalendarScreenAction.onDateSelected(date)),
+          events: snapshot.requireData.eventsMap,
+          defaultIcon: defaultIcon,
+          doneIcon: doneIcon,
+          unfinishedIcon: unfinishedIcon,
+        );
+      },
+    );
+  }
+
+  Widget _buildToDoTitle() {
     final size = Sizes.toDoTitleSize;
     return SizedBox(
       height: size.height,
@@ -58,7 +120,7 @@ final class CalendarScreen extends StatelessWidget {
           Text('To Do', style: TextStyles.toDoListTitleTextStyle),
           SizedBox(width: Sizes.defaultPaddingOfWidth),
           IconButton(
-            onPressed: () {},
+            onPressed: () => viewModel.act(CalendarScreenAction.addToDo()),
             icon: const Icon(Icons.add),
             padding: EdgeInsets.zero,
             iconSize: size.height * 0.7,
@@ -69,18 +131,33 @@ final class CalendarScreen extends StatelessWidget {
     );
   }
 
-  Widget _toDoList() {
-    return ListView.separated(
-      shrinkWrap: true,
-      primary: false,
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return const ToDoItem();
-      },
-      separatorBuilder: (context, index) {
-        return Divider(
-          height: Sizes.separatorHeight,
-          color: Colors.transparent,
+  Widget _buildToDoList() {
+    return StreamBuilder(
+      stream: viewModel.toDoList,
+      builder: (context, snapshot) {
+        return ListView.separated(
+          shrinkWrap: true,
+          primary: false,
+          itemCount: snapshot.hasData ? snapshot.requireData.length : 0,
+          itemBuilder: (context, index) {
+            if (!snapshot.hasData) return null;
+            return ToDoItem(
+              controller: TextEditingController(text: snapshot.requireData[index].title),
+              checkBoxIcon: SigppangELogoBuilder.build(
+                size: Sizes.toDoItemLogoSize,
+                state: snapshot.requireData[index].toDoState,
+              ),
+              onChecked: () {},
+              onSaved: () {},
+              onDeleted: () {},
+            );
+          },
+          separatorBuilder: (context, index) {
+            return Divider(
+              height: Sizes.separatorHeight,
+              color: Colors.transparent,
+            );
+          },
         );
       },
     );
