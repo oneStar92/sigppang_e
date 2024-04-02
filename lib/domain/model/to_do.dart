6b74d96aff6c42be39cb14daece444bd1d15598a
Ayaan <onestar7984@gmail.com>
2024-04-02 15:21:58 +0900

    refactor: ToDo Type Freezed 적용

--- lib/domain/model/to_do.dart
+++ lib/domain/model/to_do.dart
@@ -1,9 +1,16 @@
-final class ToDo {
-  final String title;
-  final bool isDone;
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
 
-  ToDo({
-    required this.title,
-    required this.isDone,
-  });
+part 'to_do.freezed.dart';
+
+part 'to_do.g.dart';
+
+@freezed
+class ToDo with _$ToDo {
+  const factory ToDo({
+    required String title,
+    required bool isDone,
+  }) = _ToDo;
+  
+  factory ToDo.fromJson(Map<String, Object?> json) => _$ToDoFromJson(json); 
 }
