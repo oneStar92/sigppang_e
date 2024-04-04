3e4b8f96e5997b3c761ecdefd436bdb4380dce4b
Ayaan <onestar7984@gmail.com>
2024-04-04 14:37:49 +0900

    refactor: SigppangELogoBuilder buildCalendarDayLogo 메소드 구현

--- lib/presentation/util/sigppang_e_logo_builder.dart
+++ lib/presentation/util/sigppang_e_logo_builder.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sigppang_e/common/constants/sizes.dart';
+import 'package:sigppang_e/domain/model/daily_status.dart';
 
 final class SigppangELogoBuilder {
   static final _readyLogo = Image.asset('assets/icons/logo_w.png');
@@ -6,6 +8,51 @@ final class SigppangELogoBuilder {
   static final _unfinishedLogo = Image.asset('assets/icons/logo_b.png');
 
   SigppangELogoBuilder._();
+
+  static Widget buildCalendarDayLogo({DailyStatus? status}) {
+    final size = Sizes.calendarDayLogoSize;
+    switch (status) {
+      case Ready():
+        return SizedBox(
+          height: size.height,
+          width: size.width,
+          child: FittedBox(child: _readyLogo),
+        );
+      case InProgress():
+        return ShaderMask(
+          shaderCallback: (rect) => LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: const [Colors.white, Colors.transparent],
+            stops: [status.progress, 1.0],
+          ).createShader(rect),
+          blendMode: BlendMode.softLight,
+          child: SizedBox(
+            height: size.height,
+            width: size.width,
+            child: FittedBox(child: _doneLogo),
+          ),
+        );
+      case Done():
+        return SizedBox(
+          height: size.height,
+          width: size.width,
+          child: FittedBox(child: _doneLogo),
+        );
+      case Unfinished():
+        return SizedBox(
+          height: size.height,
+          width: size.width,
+          child: FittedBox(child: _unfinishedLogo),
+        );
+      case null:
+        return SizedBox(
+          height: size.height,
+          width: size.width,
+          child: FittedBox(child: _readyLogo),
+        );
+    }
+  }
 
   static Widget buildReadyLogo({required Size size}) {
     return SizedBox(
