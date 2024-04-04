791edc098d65b0f715a1a4362c73c1bf0551889e
Ayaan <onestar7984@gmail.com>
2024-04-04 11:06:37 +0900

    refactor: ToDo Type createdAt 추가

--- lib/domain/model/to_do.dart
+++ lib/domain/model/to_do.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
+import 'package:sigppang_e/domain/util/timestamp_converter.dart';
 
 part 'to_do.freezed.dart';
 
@@ -10,7 +12,10 @@ class ToDo with _$ToDo {
   const factory ToDo({
     required String title,
     required bool isDone,
+    @TimestampConverter() required DateTime createdAt,
   }) = _ToDo;
-  
+
+  factory ToDo.empty() => ToDo(title: '', isDone: false, createdAt: DateTime.now());
+
   factory ToDo.fromJson(Map<String, Object?> json) => _$ToDoFromJson(json); 
 }
