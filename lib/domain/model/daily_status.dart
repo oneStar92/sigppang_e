bdfbb5cf2d5313673800781f50e85587dc620c94
Ayaan <onestar7984@gmail.com>
2024-04-04 11:05:41 +0900

    feat: DailyStatus Type 정의

---
+++ lib/domain/model/daily_status.dart
@@ -0,0 +1,24 @@
+sealed class DailyStatus {
+  factory DailyStatus.ready() = Ready;
+  factory DailyStatus.inProgress(double progress) = InProgress;
+  factory DailyStatus.done() = Done;
+  factory DailyStatus.unfinished() = Unfinished;
+}
+
+class Ready implements DailyStatus {
+  Ready();
+}
+
+class InProgress implements DailyStatus {
+  final double progress;
+
+  InProgress(this.progress);
+}
+
+class Done implements DailyStatus {
+  Done();
+}
+
+class Unfinished implements DailyStatus {
+  Unfinished();
+}
