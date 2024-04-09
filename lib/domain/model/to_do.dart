f697acfd792d4288a8407fc8de7731d818f7e463
Ayaan <onestar7984@gmail.com>
2024-04-09 19:55:08 +0900

    refactor: ToDo Type id Field 및 empty Factory Method 전달 인자 추가

--- lib/domain/model/to_do.dart
+++ lib/domain/model/to_do.dart
@@ -10,12 +10,13 @@ part 'to_do.g.dart';
 @freezed
 class ToDo with _$ToDo {
   const factory ToDo({
+    String? id,
     required String title,
     required bool isDone,
     @TimestampConverter() required DateTime createdAt,
   }) = _ToDo;
 
-  factory ToDo.empty() => ToDo(title: '', isDone: false, createdAt: DateTime.now());
+  factory ToDo.empty(DateTime date) => ToDo(title: '', isDone: false, createdAt: date);
 
   factory ToDo.fromJson(Map<String, Object?> json) => _$ToDoFromJson(json); 
 }
