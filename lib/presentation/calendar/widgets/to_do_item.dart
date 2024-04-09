2eaaa32d1c6c9ae25386514a64ea20124fe89e33
Ayaan <onestar7984@gmail.com>
2024-04-09 20:01:04 +0900

    refactor: ToDoItem 비지니스 로직 ViewModel로 분리 및 StreamBuilder 활용

--- lib/presentation/calendar/widgets/to_do_item.dart
+++ lib/presentation/calendar/widgets/to_do_item.dart
@@ -3,33 +3,20 @@ import 'package:flutter/services.dart';
 import 'package:sigppang_e/common/constants/sizes.dart';
 import 'package:sigppang_e/common/constants/text_styles.dart';
 import 'package:sigppang_e/presentation/calendar/widgets/sigppang_e_checkbox.dart';
+import 'package:sigppang_e/presentation/calendar/widgets/to_do_item_view_model.dart';
 
 final class ToDoItem extends StatelessWidget {
-  final TextEditingController _controller;
-  final FocusNode _focusNode = FocusNode();
   final String _hint = '일정을 입력해 주세요.';
-  final Widget _checkBoxIcon;
-  final Function()? _onChecked;
-  final Function()? _onDeleted;
-  final Function()? _onSaved;
+  final FocusNode _focusNode;
+  final ToDoItemViewModel _viewModel;
+  final TextEditingController _controller;
 
   ToDoItem({
     super.key,
-    required TextEditingController controller,
-    required Widget checkBoxIcon,
-    Function()? onChecked,
-    Function()? onDeleted,
-    Function()? onSaved,
-  })  : _controller = controller,
-        _checkBoxIcon = checkBoxIcon,
-        _onChecked = onChecked,
-        _onDeleted = onDeleted,
-        _onSaved = onSaved {
-    _focusNode.addListener(() {
-      if (!_focusNode.hasFocus && _controller.text.isNotEmpty) _onSaved?.call();
-      if (!_focusNode.hasFocus && _controller.text.isEmpty) _onDeleted?.call();
-    });
-  }
+    required ToDoItemViewModel viewModel,
+    required FocusNode focusNode,
+  })  : _viewModel = viewModel,
+        _controller = TextEditingController(text: viewModel.title), _focusNode = focusNode;
 
   @override
   Widget build(BuildContext context) {
@@ -41,26 +28,39 @@ final class ToDoItem extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          SigppangECheckbox(onTap: () => _onChecked?.call(), child: _checkBoxIcon),
-          SizedBox(width: Sizes.defaultPaddingOfWidth),
           Expanded(
             child: TextFormField(
               maxLines: 1,
               maxLength: 20,
               maxLengthEnforcement: MaxLengthEnforcement.enforced,
+              cursorColor: Colors.black,
               autofocus: _controller.text.isEmpty,
               controller: _controller,
               style: TextStyles.toDoItemTextStyle,
               textAlignVertical: TextAlignVertical.center,
-              decoration: InputDecoration.collapsed(hintText: _hint),
+              decoration: InputDecoration(
+                hintText: _hint,
+                border: InputBorder.none,
+                prefixIcon: StreamBuilder(
+                  stream: _viewModel.isDone,
+                  builder: (context, snapshot) {
+                    return SigppangECheckbox(
+                      onTap: () => _viewModel.act(ToDoItemAction.changeStatus()),
+                      isChecked: snapshot.data ?? false,
+                    );
+                  },
+                ),
+                prefix: SizedBox(width: Sizes.defaultPaddingOfWidth),
+              ),
               focusNode: _focusNode,
-              onTapOutside: (_) => _focusNode.unfocus(),
-              onEditingComplete: () => _focusNode.unfocus(),
+              onTapOutside: (_) => _viewModel.act(ToDoItemAction.save()),
+              onEditingComplete: () => _viewModel.act(ToDoItemAction.save()),
+              onChanged: (title) => _viewModel.act(ToDoItemAction.changeTitle(title)),
             ),
           ),
           SizedBox(width: Sizes.defaultPaddingOfWidth),
           IconButton(
-            onPressed: _onDeleted,
+            onPressed: () => _viewModel.act(ToDoItemAction.delete()),
             icon: const Icon(Icons.delete),
             padding: EdgeInsets.zero,
             iconSize: size.height * 0.7,
