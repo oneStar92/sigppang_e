ad9fb65d71ea180e0f6b1e7c630c729d75dee281
Ayaan <onestar7984@gmail.com>
2024-04-09 20:00:18 +0900

    refactor: SigppangECheckbox 기능 구현

--- lib/presentation/calendar/widgets/sigppang_e_checkbox.dart
+++ lib/presentation/calendar/widgets/sigppang_e_checkbox.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:sigppang_e/common/constants/sizes.dart';
 import 'package:sigppang_e/presentation/common/tappable_widget.dart';
+import 'package:sigppang_e/presentation/util/sigppang_e_logo_builder.dart';
 
 final class SigppangECheckbox extends TappableWidget {
-  final Widget _child;
-  const SigppangECheckbox({
+  final bool _isChecked;
+  final _checkedIcon = SigppangELogoBuilder.buildDoneLogo(size: Sizes.toDoItemLogoSize);
+  final _unCheckedIcon = SigppangELogoBuilder.buildReadyLogo(size: Sizes.toDoItemLogoSize);
+  
+  SigppangECheckbox({
     super.key,
     required super.onTap,
-    required Widget child,
-  }) : _child = child;
+    required bool isChecked,
+  }) : _isChecked = isChecked;
 
   @override
   Widget createContent(BuildContext context) {
     return AnimatedContainer(
       duration: const Duration(milliseconds: 500),
-      child: _child,
+      child: _isChecked ? _checkedIcon : _unCheckedIcon,
     );
   }
 }
