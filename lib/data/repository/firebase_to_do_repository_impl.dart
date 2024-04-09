53ef39748b228712fc3e24a5f7d53cb338c513da
Ayaan <onestar7984@gmail.com>
2024-04-09 19:58:52 +0900

    refactor: FirebaseToDoRepositoryImpl Singleton화, create, update, delete Method 기능 구현 및 readAll Mapper 사용

--- lib/data/repository/firebase_to_do_repository_impl.dart
+++ lib/data/repository/firebase_to_do_repository_impl.dart
@@ -1,19 +1,40 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sigppang_e/data/mappper/to_do_mapper.dart';
 import 'package:sigppang_e/domain/model/to_do.dart';
 import 'package:sigppang_e/domain/repository/to_do_repository.dart';
 import 'package:sigppang_e/presentation/util/auth_notifier.dart';
 
 final class FirebaseToDoRepositoryImpl implements ToDoRepository {
+  static final FirebaseToDoRepositoryImpl instance = FirebaseToDoRepositoryImpl._internal();
+
+  factory FirebaseToDoRepositoryImpl() => instance;
+
+  FirebaseToDoRepositoryImpl._internal();
+
+  final _user = AuthNotifier.instance.currentUser;
+
+  String get userId => _user != null ? _user.uid : throw Error();
+
   @override
   Stream<List<ToDo>> readAll() {
-    final user = AuthNotifier.instance.currentUser;
-    if (user != null) {
-      return FirebaseFirestore.instance
-          .collection(user.uid)
-          .snapshots()
-          .map((collection) => collection.docs.map((doc) => ToDo.fromJson(doc.data())).toList());
-    } else {
-      return Stream.error(Error());
-    }
+    return FirebaseFirestore.instance
+        .collection(userId)
+        .snapshots()
+        .map((collection) => collection.docs.map((doc) => ToDoMapper.fromDocument(doc.data(), doc.id)).toList());
+  }
+
+  @override
+  Future<void> create(ToDo toDo) {
+    return FirebaseFirestore.instance.collection(userId).doc().set(ToDoMapper.toDocument(toDo));
+  }
+
+  @override
+  Future<void> update(ToDo toDo) {
+    return FirebaseFirestore.instance.collection(userId).doc(toDo.id).update(ToDoMapper.toDocument(toDo));
+  }
+
+  @override
+  Future<void> delete(ToDo toDo) {
+    return FirebaseFirestore.instance.collection(userId).doc(toDo.id).delete();
   }
 }
