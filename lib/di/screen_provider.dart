a64167d674f2efe0a26b4349283a54d3a5f5b07c
Ayaan <onestar7984@gmail.com>
2024-04-09 20:03:02 +0900

    refactor: ScreenProvider buildCalendarScreen 및 buildToDoItem 기능 구현

--- lib/di/screen_provider.dart
+++ lib/di/screen_provider.dart
@@ -1,10 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sigppang_e/data/repository/firebase_apple_auth_repository_impl.dart';
 import 'package:sigppang_e/data/repository/firebase_google_auth_repository_impl.dart';
+import 'package:sigppang_e/data/repository/firebase_to_do_repository_impl.dart';
+import 'package:sigppang_e/domain/model/to_do.dart';
 import 'package:sigppang_e/domain/use_case/firebase_apple_login_use_case.dart';
 import 'package:sigppang_e/domain/use_case/firebase_google_login_use_case.dart';
+import 'package:sigppang_e/domain/use_case/firebase_to_do_create_use_case.dart';
+import 'package:sigppang_e/domain/use_case/firebase_to_do_delete_use_case.dart';
+import 'package:sigppang_e/domain/use_case/firebase_to_do_read_use_case.dart';
+import 'package:sigppang_e/domain/use_case/firebase_to_do_update_use_case.dart';
 import 'package:sigppang_e/presentation/calendar/calendar_view_model.dart';
 import 'package:sigppang_e/presentation/calendar/calendar_screen.dart';
+import 'package:sigppang_e/presentation/calendar/widgets/to_do_item.dart';
+import 'package:sigppang_e/presentation/calendar/widgets/to_do_item_view_model.dart';
 import 'package:sigppang_e/presentation/home/home_screen.dart';
 import 'package:sigppang_e/presentation/login/login_screen.dart';
 import 'package:sigppang_e/presentation/login/login_view_model.dart';
@@ -30,10 +39,34 @@ final class ScreenProvider {
   }
 
   static CalendarScreen buildCalendarScreen() {
-    return CalendarScreen(viewModel: CalendarViewModel(),);
+    final repository = FirebaseToDoRepositoryImpl.instance;
+    final readUseCase = FirebaseToDoReadUseCase(repository: repository);
+    return CalendarScreen(
+      viewModel: CalendarViewModel(readUseCase: readUseCase),
+    );
   }
 
   static SettingScreen buildSettingScreen() {
     return SettingScreen();
+  }
+
+  static ToDoItem buildToDoItem(ToDo toDo, {required Function(ToDo toDo) deletedOnMemory}) {
+    final repository = FirebaseToDoRepositoryImpl.instance;
+    final createUseCase = FirebaseToDoCreateUseCase(repository: repository);
+    final updateUseCase = FirebaseToDoUpdateUseCase(repository: repository);
+    final deleteUseCase = FirebaseToDoDeleteUseCase(repository: repository);
+    final focusNode = FocusNode();
+    final viewModel = ToDoItemViewModel(
+      createUseCase: createUseCase,
+      updateUseCase: updateUseCase,
+      deleteUseCase: deleteUseCase,
+      toDo: toDo,
+      unfocusAction: () => focusNode.unfocus(),
+      deleteOnMemory: deletedOnMemory,
+    );
+    return ToDoItem(
+      viewModel: viewModel,
+      focusNode: focusNode,
+    );
   }
 }
