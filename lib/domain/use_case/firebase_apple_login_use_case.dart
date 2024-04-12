b70386e17ada9bf2d820ed92a7c3201cb35d8d88
Ayaan <onestar7984@gmail.com>
2024-04-12 14:51:40 +0900

    refactor: Result Type 삭제

--- lib/domain/use_case/firebase_apple_login_use_case.dart
+++ lib/domain/use_case/firebase_apple_login_use_case.dart
@@ -1,9 +1,7 @@
-import 'package:sigppang_e/common/constants/constants.dart';
-import 'package:sigppang_e/domain/model/result.dart';
 import 'package:sigppang_e/domain/repository/auth_repository.dart';
 import 'package:sigppang_e/domain/use_case/use_case.dart';
 
-final class FirebaseAppleLoginUseCase implements UseCase<Future<Result<void>>, void> {
+final class FirebaseAppleLoginUseCase implements UseCase<Future<void>, void> {
   final AuthRepository _authRepository;
 
   FirebaseAppleLoginUseCase({
@@ -11,10 +9,7 @@ final class FirebaseAppleLoginUseCase implements UseCase<Future<Result<void>>, v
   }) : _authRepository = authRepository;
 
   @override
-  Future<Result<void>> execute({query = ()}) {
-    return _authRepository
-        .appleLogin()
-        .then((value) => Result.success(null))
-        .onError((error, stackTrace) => Result.error(loginErrorMessage));
+  Future<void> execute({query}) {
+    return _authRepository.appleLogin();
   }
 }
